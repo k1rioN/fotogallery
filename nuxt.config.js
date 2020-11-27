30d3d32433a778bfb8987385810700df7ef60f65
k1rioN <58228299+k1rioN@users.noreply.github.com>
2020-11-28 00:28:07 +0300

    dfhou

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,10 +32,15 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@nuxt/http',
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
       
+  },
+  
+  http: {
+    // proxyHeaders: false
   }
 }
