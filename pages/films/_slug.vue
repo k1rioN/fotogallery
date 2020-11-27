73d7d1399bd2bde4b743a0c9e1aa29d98748d0dc
k1rioN <58228299+k1rioN@users.noreply.github.com>
2020-11-28 00:23:09 +0300

    53232e

--- pages/films/_slug.vue
+++ pages/films/_slug.vue
@@ -1,16 +1,11 @@
 <template>
   <div>
-  
+  {{post}}
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return{
-        filmData: []
-      }
-    },
     async asyncData({ params, $http }) {
       const post = await $http.$get(`http://localhost:3000/api/actors/currentfilm/${params.slug}`)
       return { post }
